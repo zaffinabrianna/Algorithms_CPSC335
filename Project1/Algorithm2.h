750bc85b3b87a1e7421eb2a4632961e4d7b1adf8
Brianna Zaffina <83510513+zaffinabrianna@users.noreply.github.com>
2025-04-17 20:34:58 -0700

    Update Algorithm2.h

--- Project1/Algorithm2.h
+++ Project1/Algorithm2.h
@@ -11,82 +11,82 @@ class singly_list
 {
 public:
 
-  // a basic node
-  struct node
-  {
-    int data;
-    node *next; // next pointer
-  };
-
-  // insert function, for reverse function we only need insert at head & null
-  void insert(int value)
-  {
-    node *n = new node;
-    n->data = value;
-    //insert at null
-    if (head == nullptr && tail == nullptr)
+    // a basic node
+    struct node
     {
-      n->next = nullptr;
-      head = n;
-      tail = n;
+        int data;
+        node* next; // next pointer
+    };
 
-    }
-    else  //insert at head
+    // insert function, for reverse function we only need insert at head & null
+    void insert(int value)
     {
-      n->next = head; // new node's next to old head
-      head = n;       // set head
+        node* n = new node;
+        n->data = value;
+        //insert at null
+        if (head == nullptr && tail == nullptr)
+        {
+            n->next = nullptr;
+            head = n;
+            tail = n;
 
+        }
+        else  //insert at head
+        {
+            n->next = head; // new node's next to old head
+            head = n;       // set head
+
+        }
     }
-  }
 
-  //reverse the list, read the data from each node and constantly insert at head
-  singly_list reverse(){
-    node* a = head;
-    singly_list result;
-    while (a != nullptr)
+    //reverse the list, read the data from each node and constantly insert at head
+    singly_list reverse() {
+        node* a = head;
+        singly_list result;
+        while (a != nullptr)
+        {
+            result.insert(a->data);
+            a = a->next;
+        }
+        return result;
+    }
+
+
+    // add function
+    singly_list add(singly_list list1, singly_list list2, singly_list result) {
+        node* a = list1.head;
+        node* b = list2.head;
+        int carry_on = 0;    //might be 1 or 2, 3?
+        while (a != nullptr && b != nullptr)
+        {
+            int num = a->data + b->data;
+            if (carry_on > 0) {
+                result.insert(num % 10 + carry_on);
+            }
+            else
+            {
+                result.insert(num % 10);    //last digit
+            }
+            carry_on = num / 10;
+            a = a->next;
+            b = b->next;
+        }
+        result.insert(carry_on);
+        return result;
+    }
+
+    void print()
     {
-      result.insert(a->data);
-      a = a->next;
+        node* n = head;
+        while (n != nullptr)
+        {
+            cout << n->data << " "; //space is for tracing, we can also use arrow
+            n = n->next;
+        }
+        cout << endl;
     }
-    return result;
-  }
-
-
-  // add function
-  singly_list add(singly_list list1, singly_list list2, singly_list result){
-    node* a = list1.head;
-    node *b = list2.head;
-    int carry_on = 0;    //might be 1 or 2, 3?
-    while (a != nullptr && b != nullptr)
-    {
-      int num = a->data + b->data;
-      if(carry_on > 0 ){
-        result.insert(num % 10 + carry_on);
-      }
-      else
-      {
-        result.insert(num % 10);    //last digit
-      }
-      carry_on = num / 10;
-      a = a->next;
-      b = b->next;
-    }
-    result.insert(carry_on);
-    return result;
-  }
-
-  void print()
-  {
-    node *n = head;
-    while (n != nullptr)
-    {
-      cout << n->data << " "; //space is for tracing, we can also use arrow
-      n = n->next;
-    }
-    cout << endl;
-  }
 
 private:
-  node* head = nullptr; // head pointer
-  node* tail = nullptr; // tail pointer
+    node* head = nullptr; // head pointer
+    node* tail = nullptr; // tail pointer
 };
