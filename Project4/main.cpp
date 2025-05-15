f883f4510aa018ce7bc047fb098f1fcb4b1151d6
Brianna Zaffina <83510513+zaffinabrianna@users.noreply.github.com>
2025-05-15 14:20:10 -0700

    Update main.cpp

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -2,27 +2,56 @@
 
 int main()
 {
-	stockPurchase temp1;
-	vector<pair<int, int>> stocks_and_values = {
+	// PART B
+	vector<vector<int>> stocks_and_values = {
 		{ 1,2 },
 		{3, 3},
 		{5, 6},
 		{6, 7} };
 	int budget = 10;
-	int capacity = stocks_and_values.size();
 	
-	pair<int, vector<int>> result = temp1.computerMaxStockProb(budget, stocks_and_values);
+	stockPurchase temp1;
+	vector<int>results = temp1.computeMaxStock(stocks_and_values, budget);
 	
-	cout << "[" << result.first << ", [";
-	for (size_t i = 0; i < result.second.size(); ++i)
+	int totalCost = 0;
+	int totalStocks = 0;
+	
+	cout << "____________________________________________" << endl;
+	cout << "Our budget is " << budget << ".\n";
+	cout << "Let's find the total amount of stocks we can get with our budget!\n";
+	cout << "Our 2D Array: [1,2], [3,3], [5,6], [6,7]." << endl << endl;
+
+	for (int i = 0; i < results.size(); i++)
 	{
-		cout << result.second[i];
-		if (i != result.second.size() - 1)
-		{
-			cout << ", ";
-		}
-		cout << "]]" << endl;
+		int stockIndex = results[i];
+		totalCost += stocks_and_values[stockIndex][1];
+		totalStocks += stocks_and_values[stockIndex][0];
 	}
+
+	cout << "RESULTS: " << endl;
+	cout << "[" << totalCost << ", [";
+	for (int i = 0; i < results.size(); i++)
+	{
+		cout << results[i];
+		if (i != results.size() - 1)
+		{
+			cout << ",";
+		}
+	}
+	cout << "]]" << endl;
+
+	cout << "Where " << totalCost << " is the total cost, ";
+	for (int i = 0; i < results.size(); i++)
+	{
+		int stockIndex = results[i];
+		cout << "where " << stockIndex << " is the index of [" << stocks_and_values[stockIndex][0] << ", " << stocks_and_values[stockIndex][1] <<
+			"]";
+		if (i != results.size() - 1)
+		{
+			cout << " and ";
+		}
+	}
+	cout << ".\nOur maximum total stocks within our budget is: " << totalStocks << endl;
 
 	return 0;
 }
